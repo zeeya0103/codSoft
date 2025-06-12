80b61cbad6f031233407f2bba55b1d9272f23f6b
zeeya0103 <zeeya7981@gmail.com>
2025-06-12 21:02:23 +0530

    first commit

---
+++ Chat Bot/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold text-center py-8">Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
